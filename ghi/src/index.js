a60a4ee8c589bb77db1d1f611d651557fcb0e8b6
Daniel Cha <dan.hanul.cha@gmail.com>
2023-10-25 12:35:20 -0400

    pulling from latest css, still doesnt show on my end

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from "./app/store";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}></Provider>
-      <App />
+    <App />
   </React.StrictMode>
 );
 
