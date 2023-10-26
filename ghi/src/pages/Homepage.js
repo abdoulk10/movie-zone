26bbec674d115d8ccee956c7a17398c96cd9cce8
Daniel Cha <dan.hanul.cha@gmail.com>
2023-10-26 09:56:13 -0400

    undid search page for now, might revisit it, but could not get it to work despite multiple attempts

--- ghi/src/pages/Homepage.js
+++ ghi/src/pages/Homepage.js
@@ -4,34 +4,30 @@ import "../styles/Homepage.css";
 import "../App.css";
 
 function HomePage() {
-    const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=ade9ac2663bdc8bc0eae7b07d7787d12";
-    const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=ade9ac2663bdc8bc0eae7b07d7787d12&query=";
+    const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=ade9ac2663bdc8bc0eae7b07d7787d12"
+    const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=ade9ac2663bdc8bc0eae7b07d7787d12&query="
     const [movies, setMovies] = useState([]);
     const [search, setSearch] = useState("");
-    const [term, setTerm] = useState("");
 
-    useEffect(() => {
+    useEffect(() =>{
         fetch(API_URL)
             .then(response => response.json())
-            .then(data => setMovies(data.results));
-    }, []);
+            .then(data => setMovies(data.results))
+    },[])
+    console.log(movies)
 
     const handleSearch = (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         fetch(API_SEARCH + search)
             .then(response => response.json())
-            .then(data => {
-                setMovies(data.results);
-                setTerm("");
-            });
+            .then(data => setMovies(data.results));
+
+        setSearch("");
     }
 
-    const goToSearchPage = () => {
-        window.location.href = '/search';
-    }
 
-    return (
+return (
         <div className="home-page">
             <div className="search_nav">
                 <div className="title">
@@ -39,14 +35,14 @@ function HomePage() {
                 </div>
                 <div className="search_box">
                     <form onSubmit={handleSearch}>
-                        <input value={term} onChange={(e) => setTerm(e.target.value)} />
-                        <button onClick={goToSearchPage}>Search</button>
+                        <input value={search} onChange={(e) => setSearch(e.target.value)}/>
+                        <button>Search</button>
                     </form>
                 </div>
             </div>
             <div className="movies">
                 {movies?.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
+                    <MovieCard key={movie.id} movie={movie}/>
                 ))}
             </div>
         </div>
